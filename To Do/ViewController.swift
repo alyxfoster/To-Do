ccf88f6b2f71967150251a875cc82f470221e5ca
Alyx Foster <fostercodes@outlook.com>
2019-09-15 22:33:23 -0700

    Class Implementations
    Task class implemented

--- To Do/ViewController.swift
+++ To Do/ViewController.swift
@@ -8,13 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var GroupTable: UITableView!
+    
+    var tasks : [Task] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tasks = makeTasks()
+        
+        GroupTable.dataSource = self
+        GroupTable.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let task = tasks[indexPath.row]
+        if(task.priority) {cell.textLabel?.text = "!!! \(task.name)"}
+        else {cell.textLabel?.text = task.name}
+        return cell
     }
 
+    func makeTasks() -> [Task] {
+        let task1 = Task()
+        task1.name = "Complete App"
+        let task2 = Task()
+        task2.name = "Build Website"
+        task2.priority = true
+        let task3 = Task()
+        task3.name = "Grocery Shopping"
+        
+        return [task1, task2, task3]
+    }
 
 }
 
